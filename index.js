a6bf807327f64fc86775312198441cb71ebfcf8b
Ankit Pareek <ankit.p@itindia.co.in>
2021-03-05 06:50:05 +0530

    Comments added

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 
 app.use(express.json());
 
-const courses = [
+/**
+ * @dev Creating Array of objects
+ * for students 
+ */
+ const courses = [
     { 
         id : 1,
         course : 'Java'
@@ -17,26 +21,50 @@ const courses = [
         course : 'Solidity'
     }
 ];
+
+/**
+ * @dev Get method for send 
+ * response of Hello World
+ */
 app.get('/', (req, res) => {
     res.send("Hello World");
     console.log("Hello World");
 });
 
+/**
+ * @dev Get method to send the 
+ * array of no. to the client
+ */
 app.get('/api/courses', (req, res) => {
     res.send([1,2,3,4,5]);
     console.log("Array Uploaded");
 });
 
+/**
+ * @dev Get method to send the
+ * the Id which client have written
+ * at the end of the URL
+ */
 app.get('/api/courses/:id', (req, res) => {
     res.send(req.params.id);
     console.log(`Value : ${req.params.id}`);
 });
 
+/**
+ * @dev Get method to send the the 
+ * month and year in the JSON formate 
+ * to the client
+ */
 app.get('/api/courses/:year/:month', (req, res) => {
     res.send(req.params);
     console.log(`Request params : ${req.params}`);
 });
 
+/**
+ * @dev Get method to send the course 
+ * of the specified index from the courses
+ * array.
+ */
 app.get('/api/mycourses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course){  // 404 not found
@@ -47,6 +75,12 @@ app.get('/api/mycourses/:id', (req, res) => {
     }
 });
 
+/**
+ * @dev Post method for validating the 
+ * email and password which the client 
+ * have requested and server is sending 
+ * the valid response
+ */
 app.post('/api/login', (req, res) => {
      if(req.body.email == "ankit@example.com" && req.body.password == "turbo12345"){
          res.status(200).send("Login Successfull...");
@@ -57,6 +91,12 @@ app.post('/api/login', (req, res) => {
      }
 });
 
+/**
+ * @dev This will let us know 
+ * weather the PORT is running
+ * or not by simply priting the 
+ * message.
+ */
 app.listen(3000, () => {
     console.log("Listening on port...");
 });
